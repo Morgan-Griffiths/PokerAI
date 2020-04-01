98f13eb4421a324a5a9f97e63e5e4ddf6a8cd5f1
Morgan <C5ipo7i@gmail.com>
2020-03-31 19:35:58 -0700

    chore: removed folder

--- poker_scrape/poker_scrape.py
+++
@@ -1,8 +0,0 @@
-from splinter import Browser
-
-browser = Browser('firefox')
-browser.visit('https://www.facebook.com/')
-browser.fill('email', 'C5ipo7i@yahoo.com')
-browser.fill('pass', 'Darks7o2e')
-button = browser.find_by_id('u_0_d')
-button.click()
