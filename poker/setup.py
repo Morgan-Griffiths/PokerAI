8a234bbe4d1d5576ace8dd9e45ea2cb0adb2bb4e
Morgan <C5ipo7i@gmail.com>
2020-04-16 13:43:38 -0700

    chore: replaced paths

--- poker/setup.py
+++ poker/setup.py
@@ -8,6 +8,6 @@ import os
 # os.makedirs('data/predict_winner')
 # os.makedirs('data/fivecard')
 # Build data
-os.system('python cards.py -M build -d random -O data') #predict_winner
-os.system('python cards.py -M build -d handtype -O data') #/hand_types/test
+os.system('python cards.py -M build -d random -O data/predict_winner')
+os.system('python cards.py -M build -d handtype -O data/hand_types/test')
 os.system('python cards.py -M build -d handtype -m 5000 -bi 3000000 -O data/hand_types/train')
