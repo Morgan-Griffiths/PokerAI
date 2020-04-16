f2b72e375f5a8fbbd2085460a578b009efd5b7fc
Morgan <C5ipo7i@gmail.com>
2020-04-16 00:29:41 -0700

    chore: fixed paths

--- poker/setup.py
+++ poker/setup.py
@@ -7,6 +7,6 @@ os.makedirs('data/hand_types/test')
 os.makedirs('data/hand_types/train')
 os.makedirs('data/predict_winner')
 # Build data
-os.system('python cards.py -M build -d random -O data/') #data/predict_winner
-os.system('python cards.py -M build -d handtype -O data/hand_types/')
+os.system('python cards.py -M build -d random -O data/predict_winner')
+os.system('python cards.py -M build -d handtype -O data/hand_types/test')
 os.system('python cards.py -M build -d handtype -m 5000 -bi 3000000 -O data/hand_types/train')
