d39d804d960b769b60324ca1d736a654c9574a90
Morgan <c5ipo7i@yahoo.com>
2020-07-29 23:47:47 -0700

    feat: added logging feat: added batchsize agnostic value masks feat: added action prob monitoring feat: added batch learning updates

--- src/train.py
+++ src/train.py
@@ -8,13 +8,16 @@ import sys
 import numpy as np
 from pymongo import MongoClient
 from collections import defaultdict
-from hand_recognition.data_loader import datasetLoader
+from hand_recognition.data_loader import return_trajectoryloader
 import copy
 import time
+import logging
 
 from db import MongoDB
 from poker.env import Poker
 from agents.agent import ParallelAgent,FullAgent
+
+logging.basicConfig(level=logging.INFO)
 
 def pad_state(state,maxlen):
     N = maxlen - state.shape[1]
@@ -23,6 +26,7 @@ def pad_state(state,maxlen):
 
 def generate_trajectories(env,actor,training_params,id):
     """We want to store """
+    action_probs = {'SB':defaultdict(lambda:[]),'BB':defaultdict(lambda:[])}
     actor = actor.eval()
     trajectories = defaultdict(lambda:[])
     for e in range(training_params['epochs']):
@@ -32,6 +36,7 @@ def generate_trajectories(env,actor,training_params,id):
         trajectory[cur_player]['states'].append(copy.copy(state))
         trajectory[cur_player]['action_masks'].append(copy.copy(action_mask))
         trajectory[cur_player]['betsize_masks'].append(copy.copy(betsize_mask))
+        i = 0
         while not done:
             actor_outputs = actor(state,action_mask,betsize_mask)
             trajectory[cur_player]['actions'].append(actor_outputs['action'])
@@ -39,22 +44,24 @@ def generate_trajectories(env,actor,training_params,id):
             trajectory[cur_player]['action_prob'].append(actor_outputs['action_prob'])
             trajectory[cur_player]['action_probs'].append(actor_outputs['action_probs'])
             trajectory[cur_player]['betsize'].append(actor_outputs['betsize'])
+            if e == 0:
+                action_probs[cur_player][i].append(trajectory[cur_player]['action_probs'])
             state,obs,done,action_mask,betsize_mask = env.step(actor_outputs)
             cur_player = env.current_player
             if not done:
                 trajectory[cur_player]['states'].append(state)
                 trajectory[cur_player]['action_masks'].append(action_mask)
                 trajectory[cur_player]['betsize_masks'].append(betsize_mask)
+            i += 1
         assert len(trajectory[cur_player]['betsize']) == len(trajectory[cur_player]['betsize_masks'])
         rewards = env.player_rewards()
         for position in trajectory.keys():
             N = len(trajectory[position]['betsize_masks'])
             trajectory[position]['rewards'] = [rewards[position]] * N
             trajectories[position].append(trajectory[position])
-    insert_tic = time.time()
     insert_data(trajectories,env.state_mapping,env.obs_mapping,training_params['training_round'],training_params['game'],id,training_params['epochs'])
-    insert_toc = time.time()
-    print(f'insert data {insert_toc - insert_tic}')
+    print('SB',action_probs['SB'])
+    # print('BB',action_probs['BB'])
 
 def insert_data(training_data:dict,mapping:dict,obs_mapping,training_round:int,gametype:str,id:int,epochs:int):
     """
@@ -100,8 +107,7 @@ def insert_data(training_data:dict,mapping:dict,obs_mapping,training_round:int,g
     client.close()
 
 def return_value_mask(actions):
-    # print('actions',actions)
-    M = 1#actions.shape[0]
+    M = actions.shape[0]
     value_mask = torch.zeros(M,5)
     value_mask[torch.arange(M),actions] = 1
     value_mask = value_mask.bool()
@@ -112,35 +118,37 @@ def scale_rewards(self,rewards,factor=1):
     return (2 * ((rewards + self.min_reward) / (self.max_reward + self.min_reward)) - 1) * factor
 
 def learning_update(actor,critic,params):
+    log = logging.getLogger(__name__)
     actor = actor.train()
     device = params['device']
     critic_optimizer = params['critic_optimizer']
     actor_optimizer = params['actor_optimizer']
     query = {'training_round':params['training_round']}
     projection = {'state':1,'betsize_mask':1,'action_mask':1,'action':1,'reward':1,'_id':0}
-    db_tic = time.time()
     client = MongoClient('localhost', 27017,maxPoolSize=10000)
     db = client['poker']
     data = db['game_data'].find(query,projection)
-    db_toc = time.time()
-    print(f'db retrieval {db_toc - db_tic}')
-    # losses = []
-    for poker_round in data:
+    trainloader = return_trajectoryloader(data)
+    for i,data in enumerate(trainloader,1):
+        # get the inputs; data is a list of [inputs, targets]
         loop_tic = time.time()
-        torch_tic = time.time()
-        state = torch.tensor(poker_round['state'],dtype=torch.float32).to(device)
-        action = torch.tensor(poker_round['action'],dtype=torch.long).to(device)
-        reward = torch.tensor(poker_round['reward'],dtype=torch.float32).to(device)
-        betsize_mask = torch.tensor(poker_round['betsize_mask'],dtype=torch.long).to(device)
-        action_mask = torch.tensor(poker_round['action_mask'],dtype=torch.long).to(device)
-        torch_toc = time.time()
-        print(f'torch conversion {torch_toc - torch_tic}')
+        trajectory, target = data.values()
+        state = trajectory['state'].to(device)
+        action = trajectory['action'].to(device)
+        reward = target['reward'].to(device)
+        betsize_mask = trajectory['betsize_mask'].to(device)
+        action_mask = trajectory['action_mask'].to(device)
+        # state = torch.tensor(poker_round['state'],dtype=torch.float32).to(device)
+        # action = torch.tensor(poker_round['action'],dtype=torch.long).to(device)
+        # reward = torch.tensor(poker_round['reward'],dtype=torch.float32).to(device)
+        # betsize_mask = torch.tensor(poker_round['betsize_mask'],dtype=torch.long).to(device)
+        # action_mask = torch.tensor(poker_round['action_mask'],dtype=torch.long).to(device)
         ## Critic update ##
         critic_tic = time.time()
         critic_forward_tic = time.time()
         local_values = critic(state)['value']
         critic_forward_toc = time.time()
-        print(f'critic forward {critic_forward_toc - critic_forward_tic}')
+        log.debug(f'critic forward {critic_forward_toc - critic_forward_tic}')
         value_mask = return_value_mask(action)
         TD_error = local_values[value_mask] - reward
         critic_loss = (TD_error**2*0.5).mean()
@@ -149,19 +157,19 @@ def learning_update(actor,critic,params):
         critic_backward_tic = time.time()
         critic_loss.backward()
         critic_backward_toc = time.time()
-        print(f'critic backward {critic_backward_toc - critic_backward_tic}')
+        log.debug(f'critic backward {critic_backward_toc - critic_backward_tic}')
         critic_grad_tic = time.time()
         torch.nn.utils.clip_grad_norm_(critic.parameters(), params['gradient_clip'])
         critic_grad_toc = time.time()
-        print(f'critic grad {critic_grad_toc - critic_grad_tic}')
+        log.debug(f'critic grad {critic_grad_toc - critic_grad_tic}')
         critic_optimizer_tic = time.time()
         critic_optimizer.step()
         critic_optimizer_toc = time.time()
-        print(f'critic optimizer {critic_optimizer_toc - critic_optimizer_tic}')
+        log.debug(f'critic optimizer {critic_optimizer_toc - critic_optimizer_tic}')
         critic_toc = time.time()
-        print(f'critic update {critic_toc - critic_tic}')
+        log.debug(f'critic update {critic_toc - critic_tic}')
         # losses.append(critic_loss.item())
-        # print('local_values',local_values[value_mask],reward)
+        # log.debug('local_values',local_values[value_mask],reward)
         # Agent.soft_update(local_critic,target_critic,tau)
 
         # Actor update #
@@ -177,24 +185,23 @@ def learning_update(actor,critic,params):
         actor_optimizer.step()
         loop_toc = time.time()
         actor_toc = time.time()
-        print(f'actor update {actor_toc - actor_tic}')
-        print(f'learning loop {loop_toc - loop_tic}')
+        log.debug(f'actor update {actor_toc - actor_tic}')
+        log.debug(f'learning loop {loop_toc - loop_tic}')
         # Agent.soft_update(self.actor,self.target_actor,self.tau)
-        # loss_dict[i] = sum(losses)
-    # print('loss sum',sum(losses))
     return actor,critic,params
 
 def train(env,actor,critic,training_params,learning_params,id):
+    log = logging.getLogger(__name__)
     for e in range(training_params['training_epochs']):
         learning_params['training_round'] = e
         training_params['training_round'] = e
         trajectory_tic = time.time()
         generate_trajectories(env,actor,training_params,id)
         trajectory_toc = time.time()
-        print(f'trajectory time {trajectory_toc - trajectory_tic}')
+        log.debug(f'trajectory time {trajectory_toc - trajectory_tic}')
         # train on trajectories
         learning_tic = time.time()
         actor,critic,learning_params = learning_update(actor,critic,learning_params)
         learning_toc = time.time()
-        print(f'learning time {learning_toc - learning_tic}')
-        print(f'Training round {e}, ID {id}')
+        log.debug(f'learning time {learning_toc - learning_tic}')
+        log.info(f'Training round {e}, ID {id}')
