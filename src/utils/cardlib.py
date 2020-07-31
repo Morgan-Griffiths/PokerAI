47ee01a5ddca3b11793cbe3bd8a09316c1f1f15a
Morgan <c5ipo7i@yahoo.com>
2020-07-30 20:06:30 -0700

    chore: added handrank examples to assert that the handrank categories were being calculated properly

--- src/utils/cardlib.py
+++ src/utils/cardlib.py
@@ -54,10 +54,10 @@ def rank(hand):
 def test():
     # Omaha
     hand = [[14, 'c'], [2, 's'], [2, 'd'], [11, 's'],[5,'c']]
-    hand_en = [encode(c) for c in hand]
     h3 = [[7, 's'], [5, 'c'], [14, 'h'], [10, 'h']]
     h4 = [[14, 'c'], [2, 's'], [2, 'd'], [11, 's']]
     board2 = [[10, 'c'], [2, 'h'], [4, 'c'], [13, 'c'], [4, 'h']]
+    hand_en = [encode(c) for c in hand]
     en_h3 = [encode(c) for c in h3]
     en_h4 = [encode(c) for c in h4]
     en_board2 = [encode(c) for c in board2]
@@ -87,6 +87,57 @@ def test():
     en_board = [encode(c) for c in holdem_board]
     print(holdem_winner(en_hh,en_hh2,en_board))
     print(holdem_hand_rank(en_hh,en_board))
+    # Rank edges
+    holdem_hand = [[14, 'c'], [13, 'c']]
+    holdem_board = [[10, 'c'], [11, 'c'], [12, 'c'], [3, 'c'], [2, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('top str8 flush',holdem_hand_rank(en_hh,en_board))
+    holdem_hand2 = [[5, 'c'], [4, 'c']]
+    holdem_board = [[14, 'c'], [2, 'c'], [3, 'c'], [8, 'c'], [2, 'h']]
+    en_hh2 = [encode(c) for c in holdem_hand2]
+    en_board = [encode(c) for c in holdem_board]
+    print('bottom str8 flush',holdem_hand_rank(en_hh2,en_board))
+    holdem_hand = [[2, 'c'], [13, 'c']]
+    holdem_board = [[2, 'h'], [2, 's'], [2, 'd'], [4, 'c'], [5, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('bottom quad',holdem_hand_rank(en_hh,en_board))
+    holdem_hand = [[3, 'c'], [3, 'h']]
+    holdem_board = [[2, 'h'], [2, 's'], [2, 'd'], [4, 'c'], [5, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('bottom fh',holdem_hand_rank(en_hh,en_board))
+    holdem_hand = [[3, 'c'], [2, 'c']]
+    holdem_board = [[4, 'c'], [5, 'c'], [7, 'c'], [13, 'h'], [12, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('bottom flush',holdem_hand_rank(en_hh,en_board))
+    holdem_hand = [[3, 'c'], [2, 'c']]
+    holdem_board = [[14, 'c'], [5, 'h'], [4, 'c'], [13, 's'], [9, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('bottom straight',holdem_hand_rank(en_hh,en_board))
+    holdem_hand = [[3, 'c'], [2, 'c']]
+    holdem_board = [[2, 'c'], [2, 'h'], [4, 'c'], [5, 's'], [7, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('bottom trips',holdem_hand_rank(en_hh,en_board))
+    holdem_hand = [[14, 'c'], [14, 's']]
+    holdem_board = [[13, 'c'], [13, 'h'], [4, 'c'], [8, 's'], [12, 'h']]
+    en_hh = [encode(c) for c in holdem_hand]
+    en_board = [encode(c) for c in holdem_board]
+    print('top two pair',holdem_hand_rank(en_hh,en_board))
+    board2 = [[11,0],[9,1],[8,2],[4,2],[2,3]]
+    h3 = [[14,3],[14,1],[10,2],[4,3]]
+    h4 = [[14,2],[11,2],[10,0],[5,3]]
+    en_h3 = [encode(c) for c in h3]
+    en_h4 = [encode(c) for c in h4]
+    en_board2 = [encode(c) for c in board2]
+    print(en_h3,en_h4,en_board2)
+    print(winner(en_h3,en_h4,en_board2))
+    print('AAT4 vs AJT5 on J9842 Omaha hand+board ranks',hand_rank(en_h3, en_board2), hand_rank(en_h4, en_board2))
+
 
 if __name__ == "__main__":
     test()
