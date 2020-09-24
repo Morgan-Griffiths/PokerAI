13326f30520ea5cd94544bb38d2a5e760c98772f
Morgan <c5ipo7i@yahoo.com>
2020-09-24 12:52:40 -0700

    feat: refactored eval so that it pulls the same learning function as does train.

--- poker/eval_learning.py
+++ poker/eval_learning.py
@@ -8,25 +8,17 @@ import numpy as np
 import sys
 
 from db import MongoDB
-from models.model_utils import scale_rewards
 from poker_env.config import Config
 import poker_env.datatypes as pdt
 from poker_env.env import Poker
 from train import generate_trajectories,dual_learning_update,combined_learning_update
-from models.networks import CombinedNet,OmahaActor,OmahaQCritic
-
-def return_value_mask(actions):
-    M = 1#actions.shape[0]
-    value_mask = torch.zeros(M,5)
-    value_mask[torch.arange(M),actions] = 1
-    value_mask = value_mask.bool()
-    return value_mask.squeeze(0)
+from models.networks import CombinedNet,OmahaActor,OmahaQCritic,OmahaObsQCritic
+from models.model_updates import update_combined,update_actor_critic,update_critic
+from models.model_utils import scale_rewards,soft_update,hard_update,return_value_mask
 
 def eval_critic(critic,params):
-    critic_optimizer = params['critic_optimizer']  
-    device = params['device']
     query = {'training_round':0}
-    projection = {'state':1,'betsize_mask':1,'action_mask':1,'action':1,'reward':1,'_id':0}
+    projection = {'obs':1,'state':1,'betsize_mask':1,'action_mask':1,'action':1,'reward':1,'_id':0}
     client = MongoClient('localhost', 27017,maxPoolSize=10000)
     db = client['poker']
     data = list(db['game_data'].find(query,projection))
@@ -36,40 +28,20 @@ def eval_critic(critic,params):
         policy_losses = []
         for j,poker_round in enumerate(data,1):
             sys.stdout.write('\r')
-            state = poker_round['state']
-            action = poker_round['action']
-            reward = torch.tensor(poker_round['reward']).unsqueeze(-1)
-            betsize_mask = poker_round['betsize_mask']
-            action_mask = poker_round['action_mask']
-            ## Critic update ##
-            local_values = critic(state)['value']
-            value_mask = return_value_mask(action)
-            # print('local_values',local_values)
-            # print('value_mask,action',value_mask,action)
-            # print('local_values[value_mask],reward',local_values[value_mask],reward)
-            TD_error = local_values[value_mask] - reward
-            # critic_loss = (TD_error**2*0.5).mean()
-            critic_loss = F.smooth_l1_loss(reward,TD_error,reduction='sum')
-            # print('critic_loss',critic_loss)
-            critic_optimizer.zero_grad()
-            critic_loss.backward()
-            critic_optimizer.step()
-            losses.append(critic_loss.item())   
-            # Agent.soft_update(local_critic,target_critic,tau)
+            update_critic(poker_round,critic,params)
             sys.stdout.write("[%-60s] %d%%" % ('='*(60*(j)//len(data)), (100*(j)//len(data))))
             sys.stdout.flush()
             sys.stdout.write(f", round {(j):.2f}")
             sys.stdout.flush()
-            # Agent.soft_update(self.actor,self.target_actor,self.tau)
         print(f'Training Round {i}, critic loss {sum(losses)}')
     del data
 
-def eval_network_updates(actor,critic,params):
+def eval_network_updates(actor,critic,target_actor,target_critic,params):
     critic_optimizer = params['critic_optimizer']  
     actor_optimizer = params['actor_optimizer']  
     device = params['device']
     query = {'training_round':0}
-    projection = {'state':1,'betsize_mask':1,'action_mask':1,'action':1,'reward':1,'_id':0}
+    projection = {'obs':1,'state':1,'betsize_mask':1,'action_mask':1,'action':1,'reward':1,'_id':0}
     client = MongoClient('localhost', 27017,maxPoolSize=10000)
     db = client['poker']
     data = list(db['game_data'].find(query,projection))
@@ -79,57 +51,17 @@ def eval_network_updates(actor,critic,params):
         policy_losses = []
         for j,poker_round in enumerate(data,1):
             sys.stdout.write('\r')
-            state = poker_round['state']
-            action = poker_round['action']
-            reward = torch.tensor(poker_round['reward']).unsqueeze(-1)
-            betsize_mask = poker_round['betsize_mask']
-            action_mask = poker_round['action_mask']
-            ## Critic update ##
-            local_values = critic(state)['value']
-            value_mask = return_value_mask(action)
-            # print('local_values',local_values)
-            # print('value_mask,action',value_mask,action)
-            # print('local_values[value_mask],reward',local_values[value_mask],reward)
-            TD_error = local_values[value_mask] - reward
-            # critic_loss = (TD_error**2*0.5).mean()
-            critic_loss = F.smooth_l1_loss(reward,TD_error,reduction='sum')
-            # print('critic_loss',critic_loss)
-            critic_optimizer.zero_grad()
-            critic_loss.backward()
-            critic_optimizer.step()
-            losses.append(critic_loss.item())   
-            # Agent.soft_update(local_critic,target_critic,tau)
-            # Actor update #
-            target_values = critic(state)['value']
-            actor_out = actor(np.array(state),np.array(action_mask),np.array(betsize_mask))
-            actor_value_mask = return_value_mask(actor_out['action'])
-            expected_value = (actor_out['action_probs'].view(-1) * target_values.view(-1)).view(actor_value_mask.size()).detach().sum(-1)
-            advantages = (target_values[actor_value_mask] - expected_value).view(-1)
-            policy_loss = (-actor_out['action_prob'].view(-1) * advantages).sum()
-            actor_optimizer.zero_grad()
-            policy_loss.backward()
-            torch.nn.utils.clip_grad_norm_(actor.parameters(), params['gradient_clip'])
-            actor_optimizer.step()
+            critic_loss,policy_loss = update_actor_critic(poker_round,critic,target_critic,actor,target_actor,params)
+            losses.append(critic_loss)   
             policy_losses.append(policy_loss)
             sys.stdout.write("[%-60s] %d%%" % ('='*(60*(j)//len(data)), (100*(j)//len(data))))
             sys.stdout.flush()
             sys.stdout.write(f", round {(j):.2f}")
             sys.stdout.flush()
-            # Agent.soft_update(self.actor,self.target_actor,self.tau)
-        # print('\nprobs,prob,actor action,original action',actor_out['action_probs'].detach(),actor_out['action_prob'].detach(),actor_out['action'],action)
-        # print('\nlocal_values,Q_value',local_values,local_values[value_mask].item())
-        # print('\ntarget_values,target_Q_value',target_values,target_values[value_mask].item())
-        # print('\ntarget_values*mask',(actor_out['action_probs'].view(-1) * target_values.view(-1)).view(value_mask.size()))
-        # print('\nexpected_value',expected_value)
-        # print('\nadvantages',advantages)
-        # print('\nreward',reward)
-        # print('\npolicy_loss',policy_loss)
         print(f'\nTraining Round {i}, critic loss {sum(losses)}, policy loss {sum(policy_losses)}')
     del data
 
 def eval_combined_updates(model,params):
-    optimizer = params['model_optimizer']  
-    device = params['device']
     query = {'training_round':0}
     projection = {'state':1,'betsize_mask':1,'action_mask':1,'action':1,'reward':1,'_id':0}
     client = MongoClient('localhost', 27017,maxPoolSize=10000)
@@ -141,45 +73,33 @@ def eval_combined_updates(model,params):
         policy_losses = []
         for j,poker_round in enumerate(data,1):
             sys.stdout.write('\r')
-            state = poker_round['state']
-            action = poker_round['action']
-            reward = torch.tensor(poker_round['reward']).unsqueeze(-1).to(params['device'])
-            betsize_mask = poker_round['betsize_mask']
-            action_mask = poker_round['action_mask']
-            ## Critic update ##
-            local_values = model(np.array(state),np.array(action_mask),np.array(betsize_mask))['value']
-            value_mask = return_value_mask(action)
-            TD_error = local_values[value_mask] - reward
-            # critic_loss = (TD_error**2*0.5).mean()
-            critic_loss = F.smooth_l1_loss(reward,TD_error,reduction='sum')
-            # print('critic_loss',critic_loss)
-            optimizer.zero_grad()
-            critic_loss.backward()
-            optimizer.step()
-            losses.append(critic_loss.item())   
-            # Agent.soft_update(local_critic,target_critic,tau)
-            # Actor update #
-            actor_out = model(np.array(state),np.array(action_mask),np.array(betsize_mask))
-            target_values = actor_out['value']
-            actor_value_mask = return_value_mask(actor_out['action'])
-            expected_value = (actor_out['action_probs'].view(-1) * target_values.view(-1)).view(actor_value_mask.size()).detach().sum(-1)
-            advantages = (target_values[actor_value_mask] - expected_value).view(-1)
-            policy_loss = (-actor_out['action_prob'].view(-1) * advantages).sum()
-            optimizer.zero_grad()
-            policy_loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), params['gradient_clip'])
-            optimizer.step()
+            critic_loss,policy_loss = update_combined(poker_round,model,params)
+            losses.append(critic_loss) 
             policy_losses.append(policy_loss)
             sys.stdout.write("[%-60s] %d%%" % ('='*(60*(j)//len(data)), (100*(j)//len(data))))
             sys.stdout.flush()
             sys.stdout.write(f", round {(j):.2f}")
             sys.stdout.flush()
-            # Agent.soft_update(self.actor,self.target_actor,self.tau)
         print(f'Training Round {i}, critic loss {sum(losses)}, policy loss {sum(policy_losses)}')
     del data
 
 if __name__ == "__main__":
     import argparse
+
+    parser = argparse.ArgumentParser(
+        description=
+        """
+        Evaluates learning methods on static data.
+        """)
+
+    parser.add_argument('--network','-n',
+                        dest='network_type',
+                        default='dual',
+                        metavar="['combined','dual']",
+                        type=str,
+                        help='whether to split the actor critic into two separate networks or not')
+
+    args = parser.parse_args()
 
     config = Config()
     game_object = pdt.Globals.GameTypeDict[pdt.GameTypes.OMAHAHI]
@@ -243,27 +163,35 @@ if __name__ == "__main__":
         Pot: {env_params["pot"]},\
         Bettype: {env_params["bet_type"]},\
         Betsizes: {env_params["betsizes"]}')
-    print(f'Evaluating CombinedNet')
-
-    # Instantiate network
-    alphaPoker = CombinedNet(seed,nS,nA,nB,network_params).to(device)
-    alphaPoker_optimizer = optim.Adam(alphaPoker.parameters(), lr=config.agent_params['critic_lr'])
-    learning_params['model_optimizer'] = alphaPoker_optimizer
-    # actor = OmahaActor(seed,nS,nA,nB,network_params).to(device)
-    # critic = OmahaQCritic(seed,nS,nA,nB,network_params).to(device)
-    # actor_optimizer = optim.Adam(actor.parameters(), lr=config.agent_params['actor_lr'],weight_decay=config.agent_params['L2'])
-    # critic_optimizer = optim.Adam(critic.parameters(), lr=3e-5)
-    # learning_params['actor_optimizer'] = actor_optimizer
-    # learning_params['critic_optimizer'] = critic_optimizer
+    print(f'Evaluating {args.network_type}')
 
     # Clean mongo
-    # mongo = MongoDB()
-    # mongo.clean_db()
-    # mongo.close()
-    # # Gen trajectories
-    # generate_trajectories(env,actor,training_params,id=0)
+    mongo = MongoDB()
+    mongo.clean_db()
+    mongo.close()
 
-    # Eval learning models
-    # eval_network_updates(actor,critic,learning_params)
-    # eval_critic(critic,learning_params)
-    eval_combined_updates(alphaPoker,learning_params)
+    if args.network_type == 'combined':
+        # Instantiate network
+        alphaPoker = CombinedNet(seed,nS,nA,nB,network_params).to(device)
+        alphaPoker_optimizer = optim.Adam(alphaPoker.parameters(), lr=config.agent_params['critic_lr'])
+        learning_params['model_optimizer'] = alphaPoker_optimizer
+        # Gen trajectories
+        generate_trajectories(env,alphaPoker,training_params,id=0)
+        eval_combined_updates(alphaPoker,learning_params)
+    else:
+        local_actor = OmahaActor(seed,nS,nA,nB,network_params).to(device)
+        target_actor = OmahaActor(seed,nS,nA,nB,network_params).to(device)
+        local_critic = OmahaObsQCritic(seed,nS,nA,nB,network_params).to(device)
+        target_critic = OmahaObsQCritic(seed,nS,nA,nB,network_params).to(device)
+        hard_update(target_actor,local_actor)
+        hard_update(target_critic,local_critic)
+        actor_optimizer = optim.Adam(local_actor.parameters(), lr=config.agent_params['actor_lr'],weight_decay=config.agent_params['L2'])
+        critic_optimizer = optim.Adam(local_critic.parameters(), lr=3e-5)
+        learning_params['actor_optimizer'] = actor_optimizer
+        learning_params['critic_optimizer'] = critic_optimizer
+
+        # Gen trajectories
+        generate_trajectories(env,local_actor,training_params,id=0)
+        # Eval learning models
+        eval_network_updates(local_actor,local_critic,target_actor,target_critic,learning_params)
+        # eval_critic(local_critic,learning_params)
