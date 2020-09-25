77ea4fa357ab98fc8bec1357aaf3165e638c263c
Morgan <c5ipo7i@yahoo.com>
2020-09-25 12:40:05 -0700

    feat: added count_parameters which returns model info

--- poker/models/model_utils.py
+++ poker/models/model_utils.py
@@ -1,5 +1,19 @@
 import torch, os
 import numpy as np
+from prettytable import PrettyTable
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: 
+            continue
+        param = parameter.numel()
+        table.add_row([name, param])
+        total_params+=param
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
 
 def return_value_mask(actions):
     M = 1#actions.shape[0]
