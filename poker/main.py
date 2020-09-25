edc9acd893e468bd0277d393bb860dfb704da7f3
Morgan <c5ipo7i@yahoo.com>
2020-09-25 13:11:41 -0700

    chore: commented out debuggin

--- poker/main.py
+++ poker/main.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
         # for debugging
         # generate_trajectories(env,actor,training_params,id=0)
         # actor,critic,learning_params = dual_learning_update(actor,critic,target_actor,target_critic,learning_params)
-        train_dual(env,actor,critic,training_params,learning_params,id=0)
+        # train_dual(env,actor,critic,target_actor,target_critic,training_params,learning_params,id=0)
         for id in range(num_processes): # No. of processes
             p = mp.Process(target=train_dual, args=(env,actor,critic,target_actor,target_critic,training_params,learning_params,id))
             p.start()
