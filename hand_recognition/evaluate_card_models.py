c784a0fb2655324bd87bcc30c31ea80f3a06de8c
Morgan <c5ipo7i@yahoo.com>
2020-09-29 13:37:27 -0700

    chore: gpu

--- hand_recognition/evaluate_card_models.py
+++ hand_recognition/evaluate_card_models.py
@@ -30,6 +30,7 @@ def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
+    mp.set_start_method('spawn')
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
