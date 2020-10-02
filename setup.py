97f7df1ebf490664acfc3f6453f9584aeb8350b7
Morgan <c5ipo7i@yahoo.com>
2020-10-02 14:17:45 -0700

    chore: added dir

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 # Make dirs
 Path('poker/assets').mkdir(parents=True, exist_ok=True)
+Path('poker/checkpoints/frozen_layers').mkdir(parents=True, exist_ok=True)
 Path('poker/checkpoints/training_run/actor').mkdir(parents=True, exist_ok=True)
 Path('poker/checkpoints/training_run/critic').mkdir(parents=True, exist_ok=True)
 Path('poker/checkpoints/production').mkdir(parents=True, exist_ok=True)
