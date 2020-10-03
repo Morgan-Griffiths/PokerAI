27813c470b543f71f253fa183617f538bb1bc710
Morgan <c5ipo7i@yahoo.com>
2020-10-02 17:30:01 -0700

    chore: uncommented parallel training

--- poker/main.py
+++ poker/main.py
@@ -195,17 +195,17 @@ if __name__ == "__main__":
         # for debugging
         # generate_trajectories(env,actor,training_params,id=0)
         # actor,critic,learning_params = dual_learning_update(actor,critic,target_actor,target_critic,learning_params)
-        train_dual(env,actor,critic,target_actor,target_critic,training_params,learning_params,id=0)
-        # for e in range(training_params['lr_steps']):
-        #     for id in range(num_processes): # No. of processes
-        #         p = mp.Process(target=train_dual, args=(env,actor,critic,target_actor,target_critic,training_params,learning_params,id))
-        #         p.start()
-        #         processes.append(p)
-        #     for p in processes: 
-        #         p.join()
-        #     learning_params['actor_lrscheduler'].step()
-        #     learning_params['critic_lrscheduler'].step()
-        #     training_params['training_round'] = (e+1) * training_params['training_epochs']
+        # train_dual(env,actor,critic,target_actor,target_critic,training_params,learning_params,id=0)
+        for e in range(training_params['lr_steps']):
+            for id in range(num_processes): # No. of processes
+                p = mp.Process(target=train_dual, args=(env,actor,critic,target_actor,target_critic,training_params,learning_params,id))
+                p.start()
+                processes.append(p)
+            for p in processes: 
+                p.join()
+            learning_params['actor_lrscheduler'].step()
+            learning_params['critic_lrscheduler'].step()
+            training_params['training_round'] = (e+1) * training_params['training_epochs']
         # save weights
         torch.save(actor.state_dict(), os.path.join(config.agent_params['actor_path'],'OmahaActorFinal'))
         torch.save(critic.state_dict(), os.path.join(config.agent_params['critic_path'],'OmahaCriticFinal'))
