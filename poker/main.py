f594bf911f46f38b605b2e3744a35b81a6d9ef26
Morgan <c5ipo7i@yahoo.com>
2020-10-07 11:08:32 -0700

    feat: added expand_conv2d to main

--- poker/main.py
+++ poker/main.py
@@ -12,7 +12,7 @@ from poker_env.env import Poker
 from db import MongoDB
 from models.network_config import NetworkConfig,CriticType
 from models.networks import OmahaActor,OmahaQCritic,OmahaObsQCritic,CombinedNet
-from models.model_utils import update_weights,hard_update
+from models.model_utils import update_weights,hard_update,expand_conv2d
 from utils.utils import unpack_shared_dict,clean_folder
 
 from torch import optim
@@ -177,8 +177,10 @@ if __name__ == "__main__":
         critic = OmahaObsQCritic(seed,nS,nA,nB,network_params).to(device)
         if args.frozen:
             # Load pretrained hand recognizer
-            update_weights(actor,network_params['actor_hand_recognizer_path'])
-            update_weights(critic,network_params['critic_hand_recognizer_path'])
+            # update_weights(actor,network_params['actor_hand_recognizer_path'])
+            # update_weights(critic,network_params['critic_hand_recognizer_path'])
+            expand_conv2d(actor,network_params['actor_hand_recognizer_path'])
+            expand_conv2d(critic,network_params['critic_hand_recognizer_path'])
         actor.summary
         critic.summary
         target_actor = OmahaActor(seed,nS,nA,nB,network_params).to(device)
@@ -194,23 +196,23 @@ if __name__ == "__main__":
         learning_params['actor_lrscheduler'] = actor_lrscheduler
         learning_params['critic_lrscheduler'] = critic_lrscheduler
         # training loop
-        actor.share_memory()
-        critic.share_memory()
-        processes = []
+        # actor.share_memory()
+        # critic.share_memory()
+        # processes = []
         # for debugging
         # generate_trajectories(env,actor,training_params,id=0)
         # actor,critic,learning_params = dual_learning_update(actor,critic,target_actor,target_critic,learning_params)
-        # train_dual(env,actor,critic,target_actor,target_critic,training_params,learning_params,id=0)
-        for e in range(training_params['lr_steps']):
-            for id in range(num_processes): # No. of processes
-                p = mp.Process(target=train_dual, args=(env,actor,critic,target_actor,target_critic,training_params,learning_params,id))
-                p.start()
-                processes.append(p)
-            for p in processes: 
-                p.join()
-            learning_params['actor_lrscheduler'].step()
-            learning_params['critic_lrscheduler'].step()
-            training_params['training_round'] = (e+1) * training_params['training_epochs']
+        train_dual(env,actor,critic,target_actor,target_critic,training_params,learning_params,id=0)
+        # for e in range(training_params['lr_steps']):
+        #     for id in range(num_processes): # No. of processes
+        #         p = mp.Process(target=train_dual, args=(env,actor,critic,target_actor,target_critic,training_params,learning_params,id))
+        #         p.start()
+        #         processes.append(p)
+        #     for p in processes: 
+        #         p.join()
+        #     learning_params['actor_lrscheduler'].step()
+        #     learning_params['critic_lrscheduler'].step()
+        #     training_params['training_round'] = (e+1) * training_params['training_epochs']
         # save weights
         torch.save(actor.state_dict(), os.path.join(config.agent_params['actor_path'],'OmahaActorFinal'))
         torch.save(critic.state_dict(), os.path.join(config.agent_params['critic_path'],'OmahaCriticFinal'))
