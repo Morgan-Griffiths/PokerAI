eab8e54cce02764ecf28c8975ebe31b804c79a78
Morgan <c5ipo7i@yahoo.com>
2020-10-22 18:53:13 -0700

    chore: val

--- hand_recognition/evaluate_card_models.py
+++ hand_recognition/evaluate_card_models.py
@@ -133,22 +133,26 @@ def train_network(data_dict,agent_params,training_params):
 
 def train_classification(dataset_params,agent_params,training_params):
     dataset = load_data(dataset_params['data_path'])
+    trainloader = return_trainloader(dataset['trainX'],dataset['trainY'],category='classification')
+    if dataset_params['datatype'] != f'{dt.DataTypes.HANDRANKSFIVE}':
+        valloader = return_trainloader(dataset['valX'],dataset['valY'],category='classification')
+        data_dict = {
+            'trainloader':trainloader,
+            'valloader':valloader
+            # 'y_handtype_indexes':y_handtype_indexes
+        }
+        print('Data shapes',dataset['trainX'].shape,dataset['trainY'].shape,dataset['valX'].shape,dataset['valY'].shape)
+    else:
+        data_dict = {
+            'trainloader':trainloader
+        }
+        print('Data shapes',dataset['trainX'].shape,dataset['trainY'].shape)
     # dataset['trainY'] = dataset['trainY'].long()
     # dataset['valY'] = dataset['valY'].long()
     # target = dt.Globals.TARGET_SET[dataset_params['datatype']]
     # y_handtype_indexes = return_ylabel_dict(dataset['valX'],dataset['valY'],target)
-    trainloader = return_trainloader(dataset['trainX'],dataset['trainY'],category='classification')
-    # valloader = return_trainloader(dataset['valX'],dataset['valY'],category='classification')
 
-    print('Data shapes',dataset['trainX'].shape,dataset['trainY'].shape)
-    # print('Data shapes',dataset['trainX'].shape,dataset['trainY'].shape,dataset['valX'].shape,dataset['valY'].shape)
     # print('Target values',np.unique(dataset['trainY'],return_counts=True),np.unique(dataset['valY'],return_counts=True))
-
-    data_dict = {
-        'trainloader':trainloader,
-        # 'valloader':valloader,
-        # 'y_handtype_indexes':y_handtype_indexes
-    }
     train_network(data_dict,agent_params,training_params)
 
 def train_regression(dataset_params,agent_params,training_params):
