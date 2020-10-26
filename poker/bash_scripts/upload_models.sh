24a7995ffb44c819accff5e924fe53f2397d8242
Morgan <c5ipo7i@yahoo.com>
2020-10-26 13:52:30 -0700

    chore: fixed model upload

--- poker/bash_scripts/upload_models.sh
+++ poker/bash_scripts/upload_models.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Uploads public folder to S3
-aws s3 cp checkpoints/training_run/actor/OmahaActorFinal s3://pokerai/actor --acl public-read --cache-control max-age=0
-aws s3 cp checkpoints/training_run/critic/OmahaCriticFinal s3://pokerai/critic --acl public-read --cache-control max-age=0
+aws s3 cp /checkpoints/training_run/actor/OmahaActorFinal s3://pokerai/actor/OmahaActorFinal --acl public-read --cache-control max-age=0
+aws s3 cp /checkpoints/training_run/critic/OmahaCriticFinal s3://pokerai/critic/OmahaCriticFinal --acl public-read --cache-control max-age=0
