d94a4209b04776f05f75643c9302bb019aa97341
Morgan <c5ipo7i@yahoo.com>
2020-10-25 22:19:32 -0700

    chore: renamed and updated bash scripts

--- poker/bash_scripts/download_frozen.sh
+++ poker/bash_scripts/download_frozen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-aws s3 sync s3://pokerai/frozen checkpoints/frozen_layers --no-sign-request
+aws s3 sync s3://pokerai/frozen ${PWD}/checkpoints/frozen_layers --no-sign-request
 # aws s3 cp s3://pokerai/frozen/hand_board_weights checkpoints/frozen_layers/HandRankClassificationFive --no-sign-request
