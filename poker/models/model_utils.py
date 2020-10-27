30ee55b2eb96b58c35bd3a54eb041482abe3d429
Morgan <c5ipo7i@yahoo.com>
2020-10-26 21:42:19 -0700

    chore: copy_weights fix

--- poker/models/model_utils.py
+++ poker/models/model_utils.py
@@ -74,7 +74,7 @@ def expand_conv2d(network,path):
                 param.requires_grad = False
 
 def copy_weights(network,path):
-    path = '/Users/morgan/Code/PokerAI/poker/checkpoints/frozen_layers/hand_board_weights'
+    # path = '/Users/morgan/Code/PokerAI/poker/checkpoints/frozen_layers/hand_board_weights'
     if torch.cuda.is_available():
         layer_weights = torch.load(path)
     else:
