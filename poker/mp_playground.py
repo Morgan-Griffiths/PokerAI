61019a18095eab3ee0c57186bda703d7135bbc24
Morgan <c5ipo7i@yahoo.com>
2020-11-09 20:54:44 -0800

    chore: example

--- poker/mp_playground.py
+++ poker/mp_playground.py
@@ -20,6 +20,36 @@ from tournament import tournament,print_stats,eval_latest
 from torch import optim
 
 
+def example(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    # create default process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+    # create local model
+    model = nn.Linear(10, 10).to(rank)
+    # construct DDP model
+    ddp_model = DDP(model, device_ids=[rank])
+    # define loss function and optimizer
+    loss_fn = nn.MSELoss()
+    optimizer = optim.SGD(ddp_model.parameters(), lr=0.001)
+    # forward pass
+    outputs = ddp_model(torch.randn(20, 10).to(rank))
+    labels = torch.randn(20, 10).to(rank)
+    # backward pass
+    loss_fn(outputs, labels).backward()
+    # update parameters
+    optimizer.step()
+    cleanup()
+
+def main():
+    world_size = 2
+    mp.spawn(example,
+        args=(world_size,),
+        nprocs=world_size,
+        join=True)
+
+
+
 if __name__ == '__main__':
     EPOCHS = 1
 
@@ -90,7 +120,8 @@ if __name__ == '__main__':
         'koth':False
     }
     num_processes = min(1,num_gpus)
-    mp.spawn(train_test,
-    args=(env,training_params,learning_params,network_params,validation_params,),
-    nprocs=num_processes,
-    join=True)
+    main()
+    # mp.spawn(train_test,
+    # args=(env,training_params,learning_params,network_params,validation_params,),
+    # nprocs=num_processes,
+    # join=True)
