97994d76954c602633982fa61f6dc03af022d659
Morgan <c5ipo7i@yahoo.com>
2020-11-10 13:18:02 -0800

    core

--- poker/train.py
+++ poker/train.py
@@ -199,8 +199,9 @@ def dual_learning_update(id,actor,critic,target_actor,target_critic,params,valid
     for i in range(params['learning_rounds']):
         for poker_round in data:
             update_actor_critic(poker_round,critic,target_critic,actor,target_actor,params)
-        soft_update(critic,target_critic,params['device'])
-        soft_update(actor,target_actor,params['device'])
+        if id == 1:
+            soft_update(critic,target_critic,params['device'])
+            soft_update(actor,target_actor,params['device'])
     mongo.close()
 
 def batch_learning_update(actor,critic,target_actor,target_critic,params):
