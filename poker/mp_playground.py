586d88f2b09326448ce4e3af4957a3df1dfb63ba
Morgan <c5ipo7i@yahoo.com>
2020-11-09 22:03:44 -0800

    chore: added setup and cleanup

--- poker/mp_playground.py
+++ poker/mp_playground.py
@@ -22,6 +22,14 @@ from tournament import tournament,print_stats,eval_latest
 
 from torch import optim
 
+def setup_world(rank,world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+    # initialize the process group
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+def cleanup():
+    dist.destroy_process_group()
 
 def example(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
